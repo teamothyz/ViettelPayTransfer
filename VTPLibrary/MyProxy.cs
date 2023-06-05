5cc07034c6b4235a8ba5fbdac9d0f9ce3d1d9090
Tuan <zzzh0td0gzzz@gmail.com>
2023-06-06 01:01:44 +0700

    Add project files.

---
+++ VTPLibrary/MyProxy.cs
@@ -0,0 +1,13 @@
+﻿namespace VTPLibrary
+{
+    public class MyProxy
+    {
+        public string Host { get; set; } = null!;
+
+        public int Port { get; set; }
+
+        public string? Username { get; set; }
+
+        public string? Password { get; set; }
+    }
+}
